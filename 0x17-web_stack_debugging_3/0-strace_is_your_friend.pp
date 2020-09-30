b33bd2ba58895dfa2f1e021aba0bc30db5ed028c
scan3ls <scan3ls@gmail.com>
2020-09-30 15:09:17 +0000

    Fix typo in script for fixing typos

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Use puppet to fix a typo
 exec { 'typo_fixer':
   path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => "sed -i 's/.phpp/.php/ /var/www/html/wp-settings.php'",
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
 }
