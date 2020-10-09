264c8e0f8e3123bcf663f92930472ebeda8e5903
scan3ls <scan3ls@gmail.com>
2020-10-09 18:52:39 +0000

    Add task

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# PLD via Jill Rogers(Jilroge7): remove limit for requests
+exec { 'comment out limit':
+    command => "sed -i 's/ULIMIT=/# ULIMIT=/g' /etc/default/nginx",
+    path    => [ '/usr/bin', '/bin', '/usr/sbin', '/sbin' ],
+}
+exec { 'nginx restart':
+    command => 'sudo 'service nginx restart',
+    path    => 'usr/bin',
+} ~>
+service { 'nginx':
+    ensure  => running,
+}
